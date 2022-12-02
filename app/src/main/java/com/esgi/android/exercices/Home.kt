e4c4e7d07036a17b5bd38dc8333b5273ca91a979
Aristide Fumo <aristide.fumo@sopth.com>
2022-12-02 16:50:44 +0100

    first finish

---
+++ app/src/main/java/com/esgi/android/exercices/Home.kt
@@ -0,0 +1,15 @@
+package com.esgi.android.exercices
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Home : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO Changer cette ligne en fonction des layouts
+        setContentView(R.layout.ativity_home)
+
+
+    }
+}
--- app/src/main/java/com/esgi/android/exercices/MainActivity.kt
+++ app/src/main/java/com/esgi/android/exercices/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.esgi.android.exercices
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // TODO Changer cette ligne en fonction des layouts
         setContentView(R.layout.activity_main)
+        changeActivity()
+
     }
+
+    private fun changeActivity() {
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
